7525241842bff0c5f1aa6ef7ae38896fa6301561
Kevin Ringuette <kevinringuette0@gmail.com>
2025-12-18 01:00:59 -0800

    Deploy via GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+export default defineConfig(() => ({
+  base: process.env.VITE_BASE ?? '/',
+  plugins: [react(), tailwindcss()],
+}))
